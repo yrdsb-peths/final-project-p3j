6f235e2102cb1b170f9491bbc5857482e9f46eb4
Please Pick a Name <92796946+349767970@users.noreply.github.com>
2022-01-10 10:30:46 -0500

    well it is recursive, i guess

---
+++ Monster_Basic.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Monster_Basic here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Monster_Basic extends Enemy
+{
+    public Monster_Basic(){
+        setImage("monster_placeholder.png"); // The initial sprite of the character
+    }
+    
+    /**
+     * Act - do whatever the Monster_Basic wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act(){
+        // Add your action code here.
+    }
+}
--- gameMap.java
+++ gameMap.java
@@ -1,21 +1,35 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.*;
 /**
  * Write a description of class GameWorld here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class gameMap extends World
-{
-
+public class gameMap extends World{
     /**
      * Constructor for objects of class GameWorld.
      * 
      */
-    public gameMap()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+    public gameMap(){    
         super(900, 600, 1); 
+        Player player = new Player();
+        addObject(player, getWidth()/2, getHeight()/2);
+        
+    }
+    public void act(){
+        if (Greenfoot.isKeyDown("e")){
+            //change worlds
+            recursive_mod_spawn(Integer.parseInt(Greenfoot.ask("how many")));
+        }
+    }
+    public void recursive_mod_spawn(int times){
+        //why the hell am i doing this garbage, this can be done in a goddanm
+        //for loop, yet we going reursive for it, bruh
+        if(times<=0) return;
+        Monster_Basic mob = new Monster_Basic();
+        addObject(mob, Greenfoot.getRandomNumber(900), Greenfoot.getRandomNumber(600));
+        
+        recursive_mod_spawn(times-1);
     }
 }
