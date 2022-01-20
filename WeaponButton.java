38a2d3189443c2eb00a9168990fd70d0501d5e35
nickperez16 <349821439@gapps.yrdsb.ca>
2022-01-20 09:42:50 -0500

    fixed minor weapon bug
    wouldn't reset upgrade when game reset

--- WeaponButton.java
+++ WeaponButton.java
@@ -9,9 +9,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class WeaponButton extends Actor{
     public static int weaponUpgrade = 1;
     public WeaponButton(){
+        weaponUpgrade = 1;
         setImage(new GreenfootImage("Weapon\nUpgrade", 25, Color.BLACK, new Color(200,200,200)));
     }
+
     public void act(){
+
         if(weaponUpgrade < 3){
             if(Greenfoot.mousePressed(this) && Money.money >= 150){
                 Money.money -= 150;
